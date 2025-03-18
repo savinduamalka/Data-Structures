1be1374b4ceb999eb1c1d961a657826012c89bcb
savinduamalka <savinduamalka@gmail.com>
2025-03-18 22:11:18 +0530

    implement createNode function

--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -6,6 +6,20 @@ struct Node{
 	struct Node *next;
 };
 
+struct Node* createNode(int data){
+	struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+
+	if(!newNode){ // check whether there is an error in memory allocation
+		printf("Memory Error\n");
+		return NULL;
+	}
+
+	newNode->data = data;
+	newNode->next = NULL;
+	
+	return newNode;
+}
+
 int main(){
 	
 	return 0;
