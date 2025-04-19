9666cbe17f3ce4c79b45a9241338a88d4db4802c
savinduamalka <savinduamalka@gmail.com>
2025-04-19 23:41:39 +0530

    add a function to reverse the array

--- Arrays/main.c
+++ Arrays/main.c
@@ -39,6 +39,17 @@ void deleteAtGivenPosition(int arr[], int *size, int pos) {
     printf("Element at position %d deleted successfully.\n", pos);
 }
 
+void reverseArray(int arr[], int size) {
+    int temp;
+
+    for (int i = 0; i < size / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+    
+}
+
 int main() {
     const int MAX = 50;
     int arr[MAX];
