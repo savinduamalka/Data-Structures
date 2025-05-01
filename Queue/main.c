ec59d1a3b75ab545160d88fc36f9f4a9cc6dcad1
savinduamalka <savinduamalka@gmail.com>
2025-05-01 18:10:38 +0530

    implement enqueue function

--- Queue/main.c
+++ Queue/main.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1;
+int rear = -1;
+
+void enqueue(int value) {
+
+  if (rear == SIZE - 1)
+      printf("Queue is full (Overflow)\n");
+  else {
+      if (front == -1) front = 0; 
+      rear++;
+      queue[rear] = value;
+      printf("Inserted %d\n", value);
+  }
+
+}
 
 int main(){
 
