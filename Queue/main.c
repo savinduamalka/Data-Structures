310e447d49b59a01c62d16465c2bfea9de3b7b21
savinduamalka <savinduamalka@gmail.com>
2025-05-05 16:22:16 +0530

    implement a function to check if the queue is empty

--- Queue/main.c
+++ Queue/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h> 
 #define SIZE 5
 
 int queue[SIZE];
 int front = -1;
 int rear = -1;
 
+
+bool isEmpty() {
+    return front == -1;
+}
+
 void enqueue(int value) {
 
   if (rear == SIZE - 1)
       printf("Queue is full (Overflow)\n");
   else {
-      if (front == -1) front = 0; 
+      if (isEmpty()) front = 0; 
       rear++;
       queue[rear] = value;
       printf("Inserted %d\n", value);
@@ -20,12 +26,12 @@ void enqueue(int value) {
 
 void dequeue() {
 
-  if (front == -1 || front > rear) {
+  if (isEmpty()) { 
       printf("Queue is empty (Underflow)\n");
   } else {
       printf("Removed %d\n", queue[front]);
       front++;
-      if (front > rear) { 
+      if (front > rear) {
           front = -1;
           rear = -1;
       }
@@ -33,7 +39,8 @@ void dequeue() {
 }
 
 void peek() {
-  if (front == -1 || front > rear) {
+
+  if (isEmpty()) { 
       printf("Queue is empty\n");
   } else {
       printf("Front element is: %d\n", queue[front]);
@@ -42,7 +49,8 @@ void peek() {
 }
 
 void display() {
-  if (front == -1 || front > rear) {
+
+  if (isEmpty()) {
       printf("Queue is empty\n");
   } else {
       printf("Queue elements are: ");
@@ -58,7 +66,11 @@ void display() {
 int main(){
 
   printf("Hello DSA Queue....\n");
-  
+
+  if (isEmpty()) {
+      printf("Queue is initially empty.\n");
+  }
+
   enqueue(10);
   enqueue(20);
   enqueue(30);
@@ -73,10 +85,15 @@ int main(){
 
   dequeue();
   dequeue();
-  dequeue();
+
+  if (isEmpty()) {
+      printf("Queue became empty after dequeues.\n");
+  }
+
+  dequeue(); 
   peek();
 
   display();
-  
+
   return 0;
 }
