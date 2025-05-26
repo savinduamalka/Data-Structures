a28b2fd23a7d61df69c4139d8c5921921f07befb
savinduamalka <savinduamalka@gmail.com>
2025-05-26 22:12:04 +0530

    refactor main function to include user interaction for stack operations

--- Stack/Linked List of Stacks/main.c
+++ Stack/Linked List of Stacks/main.c
@@ -56,18 +56,46 @@ void peek() {
     printf("Top element is %d\n", top->data);
 }
 
-int main(){
-  push(1);
-  push(2);
-  push(3);
-  push(4);
-  push(5);
-
-  print();
-  pop();
-
-  print();
-  peek();
-
-  return 0;
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\nStack Operations:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Print\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input. Exiting.\n");
+            break;
+        }
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                if (scanf("%d", &value) != 1) {
+                    printf("Invalid input.\n");
+                    // Clear input buffer
+                    int c; while ((c = getchar()) != '\n' && c != EOF);
+                    break;
+                }
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                print();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
 }
