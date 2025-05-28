614ad511233bfa97c75bdefc0debd78e8af569b3
savinduamalka <savinduamalka@gmail.com>
2025-05-28 17:05:23 +0530

    implement node structure for the queue

--- Queue/Linked List of Queues/main.c
+++ Queue/Linked List of Queues/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct QueueNode {
+    int data;
+    struct QueueNode* next;
+} QueueNode;
+
 int main(){
 
   printf("Linked List of Queues\n");
