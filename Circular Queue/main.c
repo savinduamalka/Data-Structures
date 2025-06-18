dd080b599f17fb0752188eec5775acc69b882873
savinduamalka <savinduamalka@gmail.com>
2025-06-18 12:55:37 +0530

    enqueue, dequeue, and printQueue calls in main function

--- Circular Queue/main.c
+++ Circular Queue/main.c
@@ -55,5 +55,15 @@ void printQueue(){
 }
 int main(){
 
+  enqueue(1);
+  enqueue(2);
+  enqueue(3);
+  enqueue(4);
+  enqueue(5);
+  printQueue();
+  dequeue();
+  printQueue();
+  enqueue(6);
+  printQueue();
   return 0;
 }
