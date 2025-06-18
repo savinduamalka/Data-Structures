3e2be5592effe1282729e1be085aeaf82c12c78d
savinduamalka <savinduamalka@gmail.com>
2025-06-18 12:45:34 +0530

    implement enqueue function for circular queue

--- Circular Queue/main.c
+++ Circular Queue/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1;
+int rear = -1;
+
+void enqueue(int value) {
+  if((rear+1)%SIZE==front){
+    printf("Queue is full (Overflow)\n");
+    return;
+  }
+
+  if(front==-1){
+    front=0;
+  }
+
+  rear=(rear+1)%SIZE;
+  queue[rear]=value;
+  printf("Inserted %d\n",value);
+}
 
 int main(){
 
